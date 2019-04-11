8a654b9db3183d752eb69d2c0321b80aba8cbb40
Claista Guibert <gclaista@gmail.com>
2019-04-11 20:06:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function deleteFromObjectByKey(object, key) {
   return delete newObject.key
 }
 
-function destructivelyDeleteFromObjectByKey(object, key, value) {
-  delete object.key;
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.[key];
   return object
 }
